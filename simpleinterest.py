b509347bd75c8864088acfa2ffafca9b96b400f8
Ritik Kumar <45075455+kumarritik87@users.noreply.github.com>
2019-02-04 14:32:53 +0530

    simple interest

---
+++ simpleinterest.py
@@ -0,0 +1,8 @@
+p = int(input('enter the principal value:'))
+r = int(input('enter the rate value:'))							
+t = int(input('enter the time value:'))
+si = (p*r*t)/100
+print("Simple Interest = ",si)
+tot = p + si
+print("total amount = ",tot)
+
